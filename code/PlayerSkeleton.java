e134d49bc3ec0b53f8463ac9b0068b31fdc9f551
Qian Peisheng <a0133888@u.nus.edu>
2018-04-17 23:21:07 +0800

    randomize
    r

--- code/PlayerSkeleton.java
+++ code/PlayerSkeleton.java
@@ -5,9 +5,13 @@ import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 
 public class PlayerSkeleton {
@@ -25,7 +29,9 @@ public class PlayerSkeleton {
 
 	public static int currentRowClearedSum = 0;
 	public static double currentScore = 0;
-	public static int round = 20;
+	
+	public static int round = 50;
+	
 	public static int sleepTime = 0;
 
 	//int[] features = {colDiffSum, topHeight, numberOfRowsCleared, hasLost, numberOfHoles,meanHeightDiff,sumOfPitDepth};
@@ -37,6 +43,7 @@ public class PlayerSkeleton {
 	public static int lastRowsCleared = 0;
 	
 	public static int currentNodeValue;
+	public static int maximumRound = 2187;//3^7
 
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
@@ -66,28 +73,31 @@ public class PlayerSkeleton {
 			isUpdate[i] = indexOfNeighbour % 3;
 			indexOfNeighbour = indexOfNeighbour / 3;
 		}
+		Random rand = new Random();
 		
 		//at least 1 weight must change, so start from 1
-		for(int i = 1; i < numberOfWeights; i++) {
+		for(int i = 0; i < numberOfWeights; i++) {
 			if(isUpdate[i] == 0) {
-				//make a negative change 
+				//does not change
 				weights[i] = weights[i] ;
 			} else if(isUpdate[i] == 1){
+				//make a -ve move
 				weights[i] = weights[i] - learning_rate;
 				
 				if (weights[i] < 0 && i == 2) {
 					// weights for number of rows cleared must be positive
 					//if it goes to -ve, give it a random +ve value between 0 and 1
-					Random rand = new Random();
+
 					weights[i] = rand.nextDouble();
 				}
 			} else {
+				//make a +ve move	
 				weights[i] = weights[i] + learning_rate;
 				
 				if (weights[i] > 0 && i != 2) {
 					// weights except the one for number of rows cleared must be negative
 					//if it goes to +ve, give it a random negative value between -1 and 0
-					Random rand = new Random();
+
 					weights[i] = (-1) * rand.nextDouble();
 				}
 			}
@@ -115,6 +125,23 @@ public class PlayerSkeleton {
 			System.out.println(new DecimalFormat("##.#####").format(weights[i]));
 		}
 	}
+	
+	/**
+	 * Randomly give weights.
+	 * This is for random restart in hill climbing
+	 */
+	public void randomRestart() {
+		for (int i = 0; i < numberOfWeights; i++) {
+			Random rand = new Random();
+			if(i != 2) {
+				weights[i] = (-1) * rand.nextDouble();
+			} else {
+				weights[i] = rand.nextDouble();
+			}
+
+		}
+		updateLastweights();
+	}
 
 		
 	public static void main(String[] args) {
@@ -130,9 +157,9 @@ public class PlayerSkeleton {
 			PrintWriter writer;
 
 			writer = new PrintWriter("Log-" + formatDateTime + ".txt", "UTF-8");
+			writer.println("round: " + round);
 			writer.println("training log starts\n");
-
-
+			//writer.close();
 
 		boolean localMaximumReached = false;
 		
@@ -164,33 +191,54 @@ public class PlayerSkeleton {
 			}*/
 			currentRowClearedSum = PlayerSkeleton.currentNodeValue;
 			System.out.println("current completed "+currentRowClearedSum+" rows.");
+			
+			
 			writer.println("current completed "+currentRowClearedSum+" rows.");
-			//System.out.println("current score "+ new DecimalFormat("##.##").format(currentScore));
 			p.printWeights();
+			
 			for(int i = 0; i < numberOfWeights; i++) {
 				writer.println("w" + i +": " + weights[i]);
 			}
 			
-			int[] neighbours = new int[(int) Math.pow(3,numberOfWeights)];
-			double[] neighboursScore = new double[(int) Math.pow(3,numberOfWeights)];
+			//int[] neighbours = new int[(int) Math.pow(3,numberOfWeights)];
+			//double[] neighboursScore = new double[(int) Math.pow(3,numberOfWeights)];
 			
 			//initialize neighbours
-			for(int i = 0; i < neighbours.length; i++) {
-				neighbours[i] = 0;
-				neighboursScore[i] = 0;
-			}
+			//for(int i = 0; i < neighbours.length; i++) {
+			//	neighbours[i] = 0;
+			//	neighboursScore[i] = 0;
+			//}
 			
 
 			//for every neighbour
 			//there are 3^numberOfWeights number of neighbours
 			//i.e. every weights can increase or decrease, or dont change
 			//except that all weights do not change
-			for(int j = 0; j < Math.pow(3, numberOfWeights); j++) {
+			
+			//randomly choose neighbours, and move to the first neighbour 
+			//that is better
+			//otherwise, state that it is local maximum and decrease learning rate
+			
+			boolean isFound = false;
+			int neighbourIndex = 0;
+			int rowsCleared = 0;
+			//0 means no change. There must be some change			
+			ArrayList<Integer>  mylist = new ArrayList<Integer>();
+			for (int i = 0; i < maximumRound; i ++) {
+				mylist.add(i);
+			}
+			
+			//randomize the order
+			Collections.shuffle(mylist);
+			
+			for(neighbourIndex = 0; neighbourIndex < maximumRound; neighbourIndex ++) {
 				
 				//restore weights to the current weights
 				//then perturb one or more of them as a neighbour	
 				p.getLastweights();
-				p.updateWeights(j);
+				p.updateWeights(mylist.get(neighbourIndex));
+				
+				rowsCleared = 0;
 				//repeat a few times to and sum the result
 				for(int i = 0; i < round; i++) {
 	
@@ -209,34 +257,29 @@ public class PlayerSkeleton {
 							e.printStackTrace();
 						}
 					}
-					neighbours[j] += s.getRowsCleared();// find all neighbours of current
-					//Searcher calculator = new Searcher();
-					//neighboursScore[j] += calculator.calculateHeuristics(s);
+					rowsCleared += s.getRowsCleared();
+					//neighbours[j] += s.getRowsCleared();// find all neighbours of current
 				}
+				
+				if(rowsCleared >= currentRowClearedSum) {
+					isFound = true;
+					break;
+				}
+				
 			}
 			
-			p.getLastweights();//set back to the current node, for now
-
-		
-			// all neighbours are computed; find the maximum
-			
+			p.getLastweights();//set back to the current node, for now			
 			int bestNeighbour = -1;
 			int currentBestValue = currentRowClearedSum;
-			//System.out.println("current node "+ currentRowClearedSum+" --- ");
-
-			
-			for(int k = 0; k < neighbours.length; k++) {
-				//if a neighbour is equally good 
-				
-				if(neighbours[k] >= currentBestValue) {
-					bestNeighbour = k;
-					currentBestValue = neighbours[k];
-
-				}
+			if (isFound) {
+				bestNeighbour = mylist.get(neighbourIndex);
+				currentBestValue = rowsCleared;
 			}
+
 			PlayerSkeleton.currentNodeValue = currentBestValue;
-			if (bestNeighbour == -1) {
+			if (!isFound) {
 				System.out.println("The current node is better than all neighbours");
+				System.out.println("in random hill climbing");
 				writer.println("The current node is better than all neighbours");
 				if(learning_rate > terminate_learning_rate) {
 					learning_rate = learning_rate*learning_rate_multiplier;
@@ -253,6 +296,7 @@ public class PlayerSkeleton {
 				//set current to this neighbour, and continue the loop
 				System.out.println(bestNeighbour +" is better ");
 				writer.println(bestNeighbour +" is better ");
+				
 				p.updateWeights(bestNeighbour);
 				p.updateLastweights();// new one is better than last, so replace it
 
